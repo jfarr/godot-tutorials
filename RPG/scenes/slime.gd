bfd8d03d5d459b07daed8ce2674d2e5cf563db10
jfarr <jonothan.farr@gmail.com>
2024-03-02 16:39:55 -0800

    finish slime

--- RPG/scenes/slime.gd
+++ RPG/scenes/slime.gd
@@ -7,6 +7,9 @@ var dead = false
 var player_in_area = false
 var player = null
 
+@onready var slime_scene = $SlimeCollectible
+@export var slime_item : InventoryItem
+
 func _ready():
 	pass
 	
@@ -42,5 +45,23 @@ func die():
 	dead = true
 	$AnimatedSprite2D.play("death")
 	await get_tree().create_timer(1).timeout
+	
+	drop_slime()
+	
+	$AnimatedSprite2D.visible = false
+	$Hitbox/CollisionShape2D.disabled = true
+	$DetectionArea/CollisionShape2D.disabled = true
+
+func drop_slime():
+	slime_scene.visible = true
+	$CollectionArea/CollisionShape2D.disabled = false
+	await get_tree().create_timer(1).timeout
+	slime_collect()
+
+func slime_collect():
+	slime_scene.visible = false
+	player.collect(slime_item)
 	queue_free()
 
+func _on_collection_area_body_entered(body):
+	player = body
